4ad7769a7bd2629b9067bd0c0911a96b7c48a09f
Murik1999 <98152309+Murik1999@users.noreply.github.com>
2023-12-19 12:58:18 +0300

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,37 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Stack;
+
+class Main {
+    public static void main(String[] args) {
+        Main program = new Main();
+        program.start();
+    }
+    
+    private void start() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("laba.txt")); 
+            Stack<String> stack = new Stack<>();
+            
+            for (String line : lines) {
+                if (!line.trim().isEmpty()) { 
+                    stack.push(line);
+                }
+            }
+            
+            BufferedWriter writer = new BufferedWriter(new FileWriter("input.txt"));
+            
+            while (!stack.isEmpty()) {
+                writer.write(stack.pop() + "\n");
+            }
+            
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
